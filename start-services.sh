f87a2957d6f9d5cf0bb048dda1feb48518052c56
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2021-06-25 22:29:47 -0700

    initial commit

---
+++ start-services.sh
@@ -0,0 +1,28 @@
+echo Hello User, starting  kibana and  zeek 
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd  opt/zeek/bin
+./zeekctl start
+echo The status of zeek is below
+ 
+./zeekctl status 
+cd ..
+cd ..
+cd ..
+echo The kibana service is starting
+sudo service kibana start
+echo The status of kibana is below
+sudo service kibana status 
+
+
+
+
